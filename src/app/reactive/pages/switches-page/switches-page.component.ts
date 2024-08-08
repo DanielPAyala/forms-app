68b3138cdb6e98abfda6008a64b31b882377b4fb
Carlos Daniel <carlosporrasayala@gmail.com>
2024-08-08 00:34:22 -0500

    feat | Add a validation to a switch field

--- src/app/reactive/pages/switches-page/switches-page.component.ts
+++ src/app/reactive/pages/switches-page/switches-page.component.ts
@@ -1,9 +1,33 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   templateUrl: './switches-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class SwitchesPageComponent {
+  myform: FormGroup = this.fb.group({
+    gender: ['M', Validators.required],
+    wantNotifications: [true, Validators.required],
+    termsAndConditions: [false, Validators.requiredTrue],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  isValidFied(field: string): boolean | null {
+    return (
+      this.myform.controls[field].errors && this.myform.controls[field].touched
+    );
+  }
+
+  onSave() {
+    if (this.myform.invalid) {
+      this.myform.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myform.value);
+
+    this.myform.reset();
+  }
 }
