6b4f58724ac4bbd6195b4c9d988b4efd2e958c8b
Carlos Daniel <carlosporrasayala@gmail.com>
2024-08-08 05:33:52 -0500

    feat | Add a synchronous validation for username

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,9 +1,32 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { canBeStrider } from '../../../shared/validators/validator';
 
 @Component({
   templateUrl: './register-page.component.html',
-  styles: ``
+  styles: ``,
 })
 export class RegisterPageComponent {
+  public myForm: FormGroup = this.fb.group({
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required]],
+    username: ['', [Validators.required, canBeStrider]],
+    password: ['', [Validators.required, Validators.minLength(6)]],
+    password2: ['', [Validators.required]],
+  });
 
+  constructor(private fb: FormBuilder) {}
+
+  isValidField(field: string) {
+    // TODO: obtener validacion de un servicio
+  }
+
+  onSubmit() {
+    if (this.myForm.invalid) {
+      this.myForm.markAllAsTouched();
+      return;
+    }
+
+    console.log(this.myForm.value);
+  }
 }
